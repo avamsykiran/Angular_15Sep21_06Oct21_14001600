b671b55aedebb8494701ba66d2f86a1126f6b141
Aripaka Vamsy Kiran <a.vamc.it@gmail.com>
2021-10-07 15:43:11 +0530

    day14

--- ang-proj06/src/app/app-routing.module.ts
+++ ang-proj06/src/app/app-routing.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonsGuard } from './service/commons.guard';
+import { TxnsGuard } from './service/txns.guard';
+import { UsersGuard } from './service/users.guard';
 import { LoginComponent } from './shared/login/login.component';
 import { RegistrationComponent } from './shared/registration/registration.component';
 
 const routes: Routes = [
   {
     path: 'users', loadChildren:
-      () => import('./user-management/user-management.module').then(m => m.UserManagementModule)
+      () => import('./user-management/user-management.module').then(m => m.UserManagementModule),
+      canActivate:[UsersGuard],canActivateChild:[UsersGuard],canLoad:[UsersGuard]
   },
   {
     path: 'txns', loadChildren:
-      () => import('./transaction-management/transaction-management.module').then(m => m.TransactionManagementModule)
+      () => import('./transaction-management/transaction-management.module').then(m => m.TransactionManagementModule),
+      canActivate:[TxnsGuard],canActivateChild:[TxnsGuard],canLoad:[TxnsGuard]
   },
   {
-    path: 'signup', component:RegistrationComponent
+    path: 'signup', component:RegistrationComponent,canActivate:[CommonsGuard]
   },
   {
-    path: 'signin', component:LoginComponent
+    path: 'signin', component:LoginComponent,canActivate:[CommonsGuard]
   },
   {
     path:'',pathMatch:'full',redirectTo:'signin'
---
+++ ang-proj06/src/app/service/txns.guard.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UsersService } from './users.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TxnsGuard implements CanActivate, CanActivateChild, CanLoad {
+ 
+  constructor(private userService: UsersService, private router: Router) { }
+
+  checkPermission(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let isAllowed : boolean=true;
+
+    if(!this.userService.isLoggedIn()){
+      isAllowed=false;
+      return this.router.parseUrl("/signin");
+    }else if(this.userService.currentUser()?.role==="ADMIN"){
+      isAllowed=false;
+      return this.router.parseUrl("/users");
+    }
+
+    return isAllowed;
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkPermission();
+  }
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkPermission();
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkPermission();
+  }
+}
